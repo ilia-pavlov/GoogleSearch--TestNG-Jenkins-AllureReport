2df4864c95db6130abb12a3d814b51fb1d69bfbf
3472204455ilia <68978403+3472204455ilia@users.noreply.github.com>
2020-10-12 23:44:37 -0400

    In progress to Allure report

---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,18 @@
+package tests;
+
+import io.qameta.allure.Step;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+     @Test(priority = 0, description = ”Invalid Login Scenario with wrong username and password.”)
+
+     @Deskription (“Test Description: Login test with wrong username and wrong password.”)
+
+    @Step ("Search in Google, for method: {open} step 01 ")
+     public void open() {
+         driver.get("https://google.com");
+
+
+
+
+}
